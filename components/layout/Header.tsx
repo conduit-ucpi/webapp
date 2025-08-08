4b67895a312720abe704c30b17c97c34c55ce62e
Charlie Pank <charlie@pank.org.uk>
2025-08-08 12:27:53 +1200

    header

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -19,8 +19,13 @@ export default function Header() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center">
-            <Link href="/" className="text-xl font-bold text-green-400">
-              Conduit UCPI
+            <Link href="/" className="flex flex-col">
+              <span className="text-xl font-bold text-white">
+                Instant Escrow
+              </span>
+              <span className="text-xs text-green-400 -mt-1">
+                running on Conduit UCPI
+              </span>
             </Link>
           </div>
 

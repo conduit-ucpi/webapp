f578fd7211480668f888a04003e0b418b29fd354
Charlie Pank <charlie@pank.org.uk>
2025-08-17 14:28:09 +1200

    dispute system updated on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -156,7 +156,7 @@ export default function Home() {
                       <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                     </svg>
                   </div>
-                  <p className="text-gray-300 text-lg leading-relaxed">If there's a problem, buyer hits "dispute" - funds stay in trust until our admin team reaches a fair resolution</p>
+                  <p className="text-gray-300 text-lg leading-relaxed">If there's a problem, buyer hits "dispute", funds stay held in trust until buyer and seller can agree on refund amount in auto-dispute system</p>
                 </div>
               </div>
             </div>
